d9163ccc46953dab8767056643014448c949f6d0
Sir Bizzle VSP <rylee777@gmail.com>
2023-05-21 12:25:21 -0400

    add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'post/:id', to: 'pages#post1'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
