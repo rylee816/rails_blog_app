bf8e2ab3bd89925606675b219998e46edb0ab982
Sir Bizzle VSP <rylee777@gmail.com>
2023-10-24 17:44:47 -0400

    add scope to return current user posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@
 
 class Post < ApplicationRecord
   validates :title, :body, presence: true
+
+  scope :current_user_post, ->(user) { where(user_id: user.id) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 
 require 'faker'
 
+User.create(email: 'randyriley666@gmail.com', password: 'password')
+
 30.times do
-  Post.create(title: Faker::TvShows::GameOfThrones.character, body: Faker::Quote.most_interesting_man_in_the_world)
+  Post.create(
+    title: Faker::TvShows::GameOfThrones.character,
+    body: Faker::Quote.most_interesting_man_in_the_world,
+    user_id: User.first.id
+  )
 end
