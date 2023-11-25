93a579c7bc14a4b69ef781347783abe2040d4670
Sir Bizzle VSP <rylee777@gmail.com>
2023-11-25 11:13:22 -0500

    comment count

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,16 @@
 
 require 'faker'
 
-User.create(email: 'randyriley666@gmail.com', password: 'password', name: 'Woozle')
+user = User.create(email: 'randyriley666@gmail.com', password: 'password', name: 'Woozle')
 
 30.times do
   Post.create(
     title: Faker::TvShows::GameOfThrones.character,
     body: Faker::Quote.most_interesting_man_in_the_world,
-    user_id: User.first.id
+    user_id: user.id
   )
 end
+
+# Post.all.each do |post|
+#   Comment.create(user: user, user_id: user.id, post: post, body: Faker::Games::WorldOfWarcraft.quote)
+# end
